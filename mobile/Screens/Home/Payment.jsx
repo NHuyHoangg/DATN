64b3acf174f16aaf290f7e74fcd09e58ed18a372
Phan Hồng Đức <80117765+ducphan2408@users.noreply.github.com>
2024-05-29 19:33:22 +0700

    shipping fee

--- mobile/Screens/Home/Payment.jsx
+++ mobile/Screens/Home/Payment.jsx
@@ -33,14 +33,13 @@ import color from "../../constants/color";
 import { Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getAddress } from "../../utils/location";
+import { orderReview } from "../../utils/recharge";
 import LoadingOverlay from "../Overlay/LoadingOverlay";
 import ErrorOverlay from "../Overlay/ErrorOverlay";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 const avatarSize = screenHeight / 18;
-
-const charge = 100000;
 
 const Payment = (route) => {
   // const data = useSelector(state => state.details.item);
@@ -84,18 +83,23 @@ const Payment = (route) => {
   const [error, setError] = useState(null);
   const [change, setChange] = useState(true);
   const [address, setAddress] = useState();
+  const [shipping, setShipping] = useState();
+  const [total, setTotal] = useState();
 
   const [dataAddress, setDataAddress] = useState([]);
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true);
       try {
-        const res = await getAddress(token);
+        const res1 = await getAddress(token);
+        const res2 = await orderReview(token, route.route.params.props.id);
         // console.log(res);
-        if (res) {
-          setDataAddress(res);
-          setAddress(res.filter(item => item.is_default === 1)[0])
+        if (res1 && res2) {
+          setDataAddress(res1);
+          setAddress(res1.filter(item => item.is_default === 1)[0])
           setError(null);
+          setShipping(res2.shipping);
+          setTotal(res2.total);
         }
       } catch (error) {
         setError("Không thể tải thông tin");
@@ -137,7 +141,7 @@ const Payment = (route) => {
     return;
   }
 
-  console.log("Payment", route.route.params)
+  // console.log("Payment", route.route.params)
   // console.log(address)
 
   return (
@@ -285,7 +289,7 @@ const Payment = (route) => {
               style={{ flexDirection: "row", justifyContent: "space-between" }}
             >
               <Text style={styles.message}>Phí vận chuyển</Text>
-              <Text style={styles.message}>70.000 đ</Text>
+              <Text style={styles.message}>{new Intl.NumberFormat(['ban', 'id']).format(shipping)} đ</Text>
             </View>
 
             <View
@@ -299,7 +303,7 @@ const Payment = (route) => {
               <Text
                 style={{ fontFamily: "montserrat-semi-bold", fontSize: 16 }}
               >
-                700.000 đ
+                {new Intl.NumberFormat(['ban', 'id']).format(total)} đ
               </Text>
             </View>
           </View>
@@ -341,9 +345,9 @@ const Payment = (route) => {
           ]}
           onPress={() => {
             if (isPayment === "1")
-              navigation.navigate("WaitVerify")
+              navigation.navigate("ShoppingHistory")
             else
-              navigation.navigate("Recharge", { charge })
+              navigation.navigate("Recharge", { charge: total })
             }}
         >
           <Text style={styles.buttonText}>Xác nhận</Text>
--- mobile/utils/recharge.js
+++ mobile/utils/recharge.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { API_URL } from "@env";
 
 export const createPayment = async (token, amount) => {
-  const response = await axios.post(API_URL + "recharge/create_payment_url",
+  const response = await axios.post(
+    API_URL + "recharge/create_payment_url",
     {
       amount: amount,
       language: "vn",
@@ -16,4 +17,19 @@ export const createPayment = async (token, amount) => {
   // const message = response.data.paymentURL;
   // console.log(response.data.paymentURL);
   return response.data.paymentURL;
+};
+
+export const orderReview = async (token, id) => {
+  const response = await axios.post(API_URL + "order-review",
+    {
+      post_id: id,
+    },
+    {
+      headers: {
+        Authorization: token,
+      },
+    }
+  );
+  console.log(response.data);
+  return response.data;
 };
